265403a2f656923795a38e852ec775646d51cc5c
Joey Eikens <Joey.Eikens@gmail.com>
2022-10-04 10:20:31 -0500

    Changed markers to cirlces. Color change not working.

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -1,44 +1,60 @@
 // Creating the map object
 let myMap = L.map("map", {
-    center: [40.7, -73.95],
-    zoom: 11
+    center: [35.7, -100.95],
+    zoom:4
   });
   
   // Adding the tile layer
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(myMap);
-  
 
+// URL Link
+const url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-const url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+function chooseColor(magnitude) {
+    if (magnitude < 1) return "pink";
+    else if (magnitude < 3) return "yellow";
+    else if (magnitude < 6) return "orange";
+    else if (magnitude < 9) return "red";
+    else return "red";
+}
 
-d3.json(url).then(data => {
+function chooseSize(depth){
+
+}
+
+// var geojsonMarkerOptions = {
+//     radius: 8,
+//     fillColor: "#ff7800",
+//     color: "#000",
+//     weight: 1,
+//     opacity: 1,
+//     fillOpacity: 0.8
+// };
+
+// Get JSON Data
+d3.json(url).then(function(data) {
+
+    var geojsonMarkerOptions = {
+        radius: 8,
+        fillColor: chooseColor(data.features[0].properties.mag),
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+    };
+
+    console.log('data.features:', data.features[0].properties.mag)
     
-    console.log(data);
-
-    let markers = L.markerClusterGroup();
-
-    // create coordinate array
-    let coordinateArray = [];
-    let depthArray = [];
-
-    // Loop through JSON to create data arrays
-    for (let i = 0; i < data.length; i++) {
-
-        let location = data.features[i];
-
-        
-        if (location){
-
-            markers.addLayer([location.geometry.coordinates[1], location.geometry.coordinates[0]])
-                // .bindPopup()
+    L.geoJson(data, {
+        pointToLayer: function (feature, latlng) {
+            return L.circleMarker(latlng, geojsonMarkerOptions);
+            
         }
+    }).addTo(myMap);
 
-        coordinateArray.push(location.geometry.coordinates[0]);
-    }
 
-    myMap.addLayer(markers);
+    // console.log("fillColor:", chooseColor(feature.properties.mag))    
 
-    console.log("coordinateArray:", coordinateArray)
 });
