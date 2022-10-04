8f559aab650f2303b7e05070b028bf2e6d782cc8
Joey Eikens <Joey.Eikens@gmail.com>
2022-10-04 17:25:46 -0500

    implemented variable color and size markers

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -12,49 +12,38 @@ let myMap = L.map("map", {
 // URL Link
 const url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-function chooseColor(magnitude) {
-    if (magnitude < 1) return "pink";
-    else if (magnitude < 3) return "yellow";
-    else if (magnitude < 6) return "orange";
-    else if (magnitude < 9) return "red";
+// Function to select color base on earthquake depth
+function chooseColor(depth) {
+    if (depth < 1) return "pink";
+    else if (depth < 3) return "yellow";
+    else if (depth < 6) return "orange";
+    else if (depth < 9) return "red";
     else return "red";
 }
 
-function chooseSize(depth){
-
-}
-
-// var geojsonMarkerOptions = {
-//     radius: 8,
-//     fillColor: "#ff7800",
-//     color: "#000",
-//     weight: 1,
-//     opacity: 1,
-//     fillOpacity: 0.8
-// };
-
-// Get JSON Data
-d3.json(url).then(function(data) {
-
-    var geojsonMarkerOptions = {
-        radius: 8,
-        fillColor: chooseColor(data.features[0].properties.mag),
+// Function to style markers
+function markerStyle(feature) {
+    return{
+        radius: feature.properties.mag * 2.5,
+        fillColor: chooseColor(feature.geometry.coordinates[2]),
         color: "#000",
         weight: 1,
         opacity: 1,
         fillOpacity: 0.8
-    };
+    }
+}
 
-    console.log('data.features:', data.features[0].properties.mag)
-    
+// Function to add text to marker
+// function onEachFeature()
+
+
+// Get JSON Data
+d3.json(url).then(function(data) {
+   
     L.geoJson(data, {
+        style: markerStyle,
         pointToLayer: function (feature, latlng) {
-            return L.circleMarker(latlng, geojsonMarkerOptions);
-            
+            return L.circleMarker(latlng);
         }
     }).addTo(myMap);
-
-
-    // console.log("fillColor:", chooseColor(feature.properties.mag))    
-
 });
