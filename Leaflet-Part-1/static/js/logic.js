c4d9da7b7381f9ffc8724aabc6d6d663c624a68c
Joey Eikens <Joey.Eikens@gmail.com>
2022-10-14 10:41:02 -0500

    added legend and updated popup text

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -14,11 +14,11 @@ const url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.
 
 // Function to select color base on earthquake depth
 function chooseColor(depth) {
-    if (depth < 1) return "pink";
-    else if (depth < 3) return "yellow";
-    else if (depth < 6) return "orange";
-    else if (depth < 9) return "red";
-    else return "red";
+    if (depth <= 10) return "#ffffb2";
+    else if (depth <= 30) return "#fecc5c";
+    else if (depth <= 60) return "#fd8d3c";
+    else if (depth <= 90) return "#f03b20";
+    else return "#bd0026";
 }
 
 // Function to style markers
@@ -33,17 +33,66 @@ function markerStyle(feature) {
     }
 }
 
-// Function to add text to marker
-// function onEachFeature()
-
-
 // Get JSON Data
 d3.json(url).then(function(data) {
+
+    console.log("data", data)
    
-    L.geoJson(data, {
+    geojson = L.geoJson(data, {
         style: markerStyle,
         pointToLayer: function (feature, latlng) {
             return L.circleMarker(latlng);
-        }
+        },
+        onEachFeature: function(feature, layer) {
+        // layer.bindPopup("<h3> Location: " + feature.properties.place + "<h3>");
+        layer.bindPopup("<strong> Location: " + feature.properties.place + "</strong><br /><br />Magnitude: " +
+        feature.properties.mag + "<br /><br />Depth: " + feature.geometry.coordinates[2]);
+
+        },  
+
     }).addTo(myMap);
+
+    // for loop to generate depth list
+    let depth = []
+
+    for (let i=0; i < data.features.length; i++) {
+        let d = data.features[i].geometry.coordinates[2];
+        depth.push(d);
+    }
+
+    depth.sort((a, b) => a - b );
+
+    console.log('depth sorted', depth)
+
+    let legendArray = [Math.round(depth[0]), 15, 35, 65, Math.round(depth[depth.length -1])];
+    let coloArray = chooseColor(legendArray);
+
+    // Create a legend
+    let legend = L.control({position: 'bottomright'});
+
+    legend.onAdd = function () {
+        let div = L.DomUtil.create("div", "info legend");
+
+        // let colors = geojson.options.colors;
+        let labels = [];
+
+        console.log('labels', labels)
+
+        let legendInfo = "<div class=\"min\">" + legendArray[0] + "</div>" +
+          "<div class=\"max\">" + legendArray[legendArray.length - 1] + "</div>" +
+        "</div>";
+  
+
+        div.innerHTML = legendInfo;
+
+        legendArray.forEach(function(legendArray, index) {
+            labels.push("<li style=\"background-color: " + chooseColor(legendArray) + "\"></li>");
+        });
+  
+      div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+      return div;
+    };
+
+      // Adding the legend to the map
+    legend.addTo(myMap);
 });
